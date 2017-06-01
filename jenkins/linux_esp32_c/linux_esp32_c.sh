110a970fa4049775e8ae41c642634e0ded1e4074
Roy Sprowl <roys@jetstreamsoftware.com>
2017-05-31 17:21:19 -0700

    final checking of env

--- jenkins/linux_esp32_c/linux_esp32_c.sh
+++ jenkins/linux_esp32_c/linux_esp32_c.sh
@@ -1,8 +1,10 @@
 echo "hello esp32"
-dir /home/jenkins
-dir /home/jenkins/esp32
-# dir /home/jenkins/esp32-idf
 
  printenv
+
+dir /home/jenkins
+dir /home/jenkins/esp32
+dir /home/jenkins/esp32-idf
+
 
 
