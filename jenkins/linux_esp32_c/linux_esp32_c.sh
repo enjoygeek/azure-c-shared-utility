72eb0426ca98f099ca8e73f93c8e70d4de7484b7
Roy Sprowl <roys@jetstreamsoftware.com>
2017-06-01 14:57:19 -0700

    Nearly working

--- jenkins/linux_esp32_c/linux_esp32_c.sh
+++ jenkins/linux_esp32_c/linux_esp32_c.sh
@@ -1,22 +1,27 @@
-echo "hello esp32"
+echo "Cross-compiling a non-working ESP32 example"
 
- printenv
+export PATH=$PATH:$ESP32_TOOLS
 
- export PATH=$ESP32_TOOLS:$PATH
- pwd
- dir
- #cd test_build
- #make
+# Verify that the ESP32 SDK is at a tested commit
+pushd $IDF_PATH
+git rev-parse HEAD
+idf_commit=$(git rev-parse HEAD)
+tested_idf_commit=53893297299e207029679dc99b7fb33151bdd415
+popd
+if [ $idf_commit = $tested_idf_commit ]
+	then echo "ESP32 SDK commit is okay"
+else 
+	echo "ESP32 SDK commit is " $idf_commit
+	echo "ESP32 SDK commit should be " $tested_idf_commit
+	echo "Error: ESP32 SDK commit is wrong"
+	exit 1
+fi
 
-# dir /home/jenkins
-# dir /home/jenkins/esp32
-# dir /home/jenkins/esp32-idf
+pwd
+cd c-utility/jenkins/linux_esp32_c/test_build
+dir
+#make
 
-# cd /home/jenkins/esp32-idf
-# git rev-parse HEAD
-# git checkout 53893297299e207029679dc99b7fb33151bdd415
-# git rev-parse HEAD
-
-echo "version 01"
+echo "version 02"
 
 
