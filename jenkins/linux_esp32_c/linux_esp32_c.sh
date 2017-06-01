0711e18b6a2731d8b2eda8ce8af70a5d72513e72
Roy Sprowl <roys@jetstreamsoftware.com>
2017-06-01 11:24:42 -0700

    first make

--- jenkins/linux_esp32_c/linux_esp32_c.sh
+++ jenkins/linux_esp32_c/linux_esp32_c.sh
@@ -2,13 +2,17 @@ echo "hello esp32"
 
  printenv
 
-dir /home/jenkins
-dir /home/jenkins/esp32
-dir /home/jenkins/esp32-idf
+ export PATH=$ESP32_TOOLS:$PATH
+ cd test_build
+ make
 
-cd /home/jenkins/esp32-idf
-git rev-parse HEAD
-git checkout 972d1d9242bad8b2c09840794dd143c1d5ad75da
-git rev-parse HEAD
+# dir /home/jenkins
+# dir /home/jenkins/esp32
+# dir /home/jenkins/esp32-idf
+
+# cd /home/jenkins/esp32-idf
+# git rev-parse HEAD
+# git checkout 53893297299e207029679dc99b7fb33151bdd415
+# git rev-parse HEAD
 
 
